80be688c4fc909be2ffc6b0008e438e65eac1193
Michael Chen <michaelchen@10-249-211-119.wireless.oregonstate.edu>
2022-10-16 12:40:42 -0700

    add a unit test for Weather model

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,5 +1,7 @@
 import XCTest
 import MyLibrary
+// import WeatherService because of Weather Model
+@testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
@@ -67,6 +69,74 @@ final class MyLibraryTests: XCTestCase {
 
         // Then
         XCTAssertNil(isLuckyNumber)
+    }
+    
+    func testWeatherModule () async {
+        
+        // Given a jasonString is the same structure as the openWeather response
+        let jasonString = """
+            {
+                "coord": {
+                    "lon": 10.99,
+                    "lat": 44.34
+                },
+                "weather": [
+                    {
+                    "id": 501,
+                    "main": "Rain",
+                    "description": "moderate rain",
+                    "icon": "10d"
+                    }
+                ],
+                "base": "stations",
+                "main": {
+                    "temp": 298.48,
+                    "feels_like": 298.74,
+                    "temp_min": 297.56,
+                    "temp_max": 300.05,
+                    "pressure": 1015,
+                    "humidity": 64,
+                    "sea_level": 1015,
+                    "grnd_level": 933
+                },
+                "visibility": 10000,
+                "wind": {
+                    "speed": 0.62,
+                    "deg": 349,
+                    "gust": 1.18
+                },
+                "rain": {
+                    "1h": 3.16
+                },
+                "clouds": {
+                    "all": 100
+                },
+                "dt": 1661870592,
+                "sys": {
+                    "type": 2,
+                    "id": 2075663,
+                    "country": "IT",
+                    "sunrise": 1661834187,
+                    "sunset": 1661882248
+                },
+                "timezone": 7200,
+                "id": 3163858,
+                "name": "Zocca",
+                "cod": 200
+            }
+        """
+        
+        let data = Data(jasonString.utf8)
+        
+        do{
+            // When the jsonString can be decoded by the Weather model
+            let jsonData = try JSONDecoder().decode(Weather.self, from: data)
+            // Then get the correct temp from JSON. Test success.
+            XCTAssertEqual(jsonData.main.temp, 298.48)
+        }catch{
+            XCTAssert(false)
+        }
+
     }
 
 }
